16b67241ab4bcc1fc13005512a0a799551773e4b
Justin McManus <jmcmanu@gmail.com>
2012-11-27 19:06:17 -0800

    Refactoring. Added DEBUG conditional to php files.

--- end.php
+++ end.php
@@ -1,3 +1,14 @@
+<?php
+    $DEBUG = is_dir("../site");
+    $studies_url = "http://staff.washington.edu/rbwatson/study_results/";
+    if ($DEBUG) {
+        $studies_url = "/server/study_results/";
+    }
+
+    // todo - add error handling
+    $file = "sess" . $_GET["wlux_session"] . "cond" . $_GET["wlux_condition"] . ".txt";
+    $study_url = $studies_url . $file;
+?>
 <html>
     <head>
         <style type="text/css">
@@ -5,12 +16,8 @@
         </style>
     </head>
     <body>
-        <?php
-            // todo - add error handling
-            $file = "sess" . $_GET["wlux_session"] . "cond" . $_GET["wlux_condition"] . ".txt"
-        ?>
         <h2>WebLabUX Survey</h2>
         <p>Thank you for taking part in our survey!!</p>
-        <p>Click <a href="/server/study_results/<?=$file?>">here</a> to view the data collected.</p>
+        <p>Click <a href="<?=$study_url ?>">here</a> to view the data collected.</p>
     </body>
 </html>
--- start.php
+++ start.php
@@ -1,3 +1,44 @@
+<?php
+    $DEBUG = is_dir("../site");
+    $site_url = "http://students.washington.edu/rbwatson/hearts.html";
+    if ($DEBUG) {
+        $site_url = "/site/hearts.html";
+    }
+    $filename = "sessions.txt";
+
+    //first, obtain the data initially present in the text file
+    $ini_handle = fopen($filename, "r");
+    $old_string = fread($ini_handle, filesize($filename));
+
+    //parse string
+    $array = explode(" ", $old_string);
+    $wlux_sid = $array[0];   //session ID passed to the study site
+    $condition = $array[1];
+
+    fclose($ini_handle);
+    //done obtaining initially present data
+    $handle = fopen($filename, "w");
+
+    //new incremented session ID
+    $new_sid = $wlux_sid + 1;
+
+    //incrementing condition - 1,2,3, or 4
+    $condition = $condition + 1;
+
+    if($condition == 5)
+        $condition = 1;
+
+    //adding CSS stylesheets for the condition
+    $css = 'css/style'.$condition.'.css';
+
+    $new_string = $new_sid.' '.$condition.' '.$css."\n".$old_string;
+
+    if (fwrite($handle, $new_string) === false)
+    {
+           echo "Cannot write to text file. <br />";
+    }
+    fclose($handle);
+?>
 <html>
     <head>
         <style type="text/css">
@@ -8,49 +49,11 @@
 
     <body>
         <div class="container">
-
-            <?php
-            $filename = "sessions.txt";
-
-            //first, obtain the data initially present in the text file
-            $ini_handle = fopen($filename, "r");
-            $old_string = fread($ini_handle, filesize($filename));
-
-            //parse string
-            $array = explode(" ", $old_string);
-            $wlux_sid = $array[0];   //session ID passed to the study site
-            $condition = $array[1];
-
-            fclose($ini_handle);
-            //done obtaining initially present data
-            $handle = fopen($filename, "w");
-
-            //new incremented session ID
-            $new_sid = $wlux_sid + 1;
-
-            //incrementing condition - 1,2,3, or 4
-            $condition = $condition + 1;
-
-            if($condition == 5)
-                $condition = 1;
-
-            //adding CSS stylesheets for the condition
-            $css = 'css/style'.$condition.'.css';
-
-            $new_string = $new_sid.' '.$condition.' '.$css."\n".$old_string;
-
-            if (fwrite($handle, $new_string) === false)
-            {
-                   echo "Cannot write to text file. <br />";
-            }
-            fclose($handle);
-            ?>
-
             <h2>WebLabUX Survey Consent Form </h2>
 
             <p>Thank you for agreeing to participate in our survey.<br />
             Please click on continue to begin the survey</p>
-            <form name="form1" method="GET" action="/site/hearts.html">
+            <form name="form1" method="GET" action="<?= $site_url ?>">
                 <input type="hidden" name="wlux_session" value="<?=$wlux_sid?>" />
                 <input type="submit" value="continue" />
             </form>
